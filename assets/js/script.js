d830c54764d4a3fcf3ff7f24ec74db2e2846ca60
Nicolas Morales <mfuenzalida.ne@gmail.com>
2023-04-04 20:21:43 -0400

    agregué un tercer tooltip en los iconos, y funcionalidad con jquery a mas elementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+   //permite que se oculte la seccion de quienes somos en s y xs
     $(window).resize(function() {
         if ($(window).width() < 576) {
             $('.ocultar').hide();
@@ -6,17 +7,20 @@ $(document).ready(function(){
             $('.ocultar').show();
         }
     });
-
+//tooltip sobre los iconos en la sección de Quienes somos
     $('[data-toggle="tooltip"]').tooltip(
     
     );
-
-    $(".viajecito").dblclick(function(){
+//   dobleclick y cambio a color rojo en viajes chile
+    $(".viajecito").dblclick(function(){  
         $(this).css({
             "color":"red"
         });
         });
-    
+    //card toggle: cuando haga click sobre el anunciado "Destacados", se esconde el texto
+        $("#destacados").click(function () {
+            $(".cuerpo").toggle(500);
+        });  
     
     //fin principal
     });   
